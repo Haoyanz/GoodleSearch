5b10a8ac51b2415a0f2917da30fd61232a1a80ef
Haoyanz <haoyaz1@uci.edu>
2019-11-07 13:29:15 -0800

    id_url_map updated

--- Indexing.py
+++ Indexing.py
@@ -1,6 +1,14 @@
 import os
+import json
 
-for root, dirs, files in os.walk(directory):
+id_url_map = {}
+index = 1
+for root, dirs, files in os.walk('./DEV'):
     for file in files:
-        if file.endswith('.txt'):
-            print file
+        if file.endswith('.json'):
+            with open(os.path.join(root, file), 'r') as json_file:
+                json_obj = json.load(json_file)
+                id_url_map[index] = json_obj['url']
+                index += 1
+
+print(id_url_map)
