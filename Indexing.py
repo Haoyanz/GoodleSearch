d1c908eef4c10831625b3723759b5d2fade56db1
zsun005 <zsun005@users.noreply.github.com>
2019-11-07 16:04:30 -0800

    Add token to map

--- Indexing.py
+++ Indexing.py
@@ -25,6 +25,35 @@ def porter_stem(content):
     return words
 
 
+def beautiful_print_map(url_map):
+    for key, value in url_map.items():
+        print(key, value)
+
+
+def write_map_to_file():
+    f = open("token_map.txt", "w")
+    for key, value in token_map.items():
+        f.write(key + '\t')
+        f.write(json.dumps(value) + '\n')
+    f.close()
+
+def add_token_to_map(token_list: [], document_id: int):
+    '''
+    Here I treat token_map as a dict where key is token, and value is another dict (key is document id, and value is the
+    frequencies correspond)
+    :param token_list: list of tokens [str]
+    :param document_id: int, document id from index in url map
+    :return: None
+    '''
+    for token in token_list:
+        if token not in token_map.keys():
+            token_map[token] = {document_id: 1}
+        elif document_id in token_map[token]:
+            token_map[token][document_id] += 1
+        else:
+            token_map[token][document_id] = 1
+
+
 id_url_map = {}
 token_map = {}
 index = 1
@@ -37,12 +66,14 @@ for root, dirs, files in os.walk('./DEV'):
                 id_url_map[index] = json_obj['url']
                 # parse html for content as a whole string
                 soup = BeautifulSoup(json_obj['content'], 'html.parser')
-                texts = soup.findAll(text = True)
+                texts = soup.findAll(text=True)
                 visible_texts = filter(tag_visible, texts)
                 content = " ".join(t.strip() for t in visible_texts)
                 # porter stemming
                 token_list = porter_stem(content)
-                print (token_list)
+                # print(token_list)
+                add_token_to_map(token_list, index)
                 index += 1
 
-print(id_url_map)
+# beautiful_print_map(token_map)
+write_map_to_file()
