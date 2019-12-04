b9b6925795a2ce54b09679538efefc05bf35a892
孙泽曦 <sunzexi@dhcp-v036-052.mobile.uci.edu>
2019-12-03 18:28:10 -0800

    William

--- Indexing.py
+++ Indexing.py
@@ -1,8 +1,9 @@
 import os
 import json
 import re
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 
@@ -20,10 +21,9 @@ def porter_stem(content):
     content = (str(content)).lower()
     words = []
     words.extend(re.findall("[a-z0-9]+", content))
-    stem_words = []
-    for word in words:
-        stem_words.append(ps.stem(word))
-    return stem_words
+    for i, word in enumerate(words):
+        words[i] = ps.stem(word)
+    return words
 
 
 def beautiful_print_map(url_map):
@@ -31,17 +31,21 @@ def beautiful_print_map(url_map):
         print(key, value)
 
 
-def write_map_to_file(map, file_name):
-    f = open(file_name, "w")
-    for key in sorted (map.keys()):
-        f.write(key)
-        for little_key in sorted (map[key].keys()):
-            f.write(' ' + str(little_key) + ":" + str(map[key][little_key]))
-        f.write('\n')
+def write_doc_size_to_file(document_id, size):
+    f = open("document_size.txt", "a+")
+    f.write(str(document_id) + ' ' + str(size) + '\n')
     f.close()
 
 
-def add_token_to_map(token_list, document_id):
+def write_map_to_file():
+    f = open("token_map.txt", "w")
+    for key, value in token_map.items():
+        f.write(key + '\t')
+        f.write(json.dumps(value) + '\n')
+    f.close()
+
+
+def add_token_to_map(token_list: [], document_id: int):
     '''
     Here I treat token_map as a dict where key is token, and value is another dict (key is document id, and value is the
     frequencies correspond)
@@ -57,69 +61,50 @@ def add_token_to_map(token_list, document_id):
         else:
             token_map[token][document_id] = 1
 
-id_size_map = {}
-id_url_map = {}
-token_map = {}
-file_number = 1
-temp_index = 1
-index = 1
-for root, dirs, files in os.walk('./DEV'):
-    for file in files:
-        if file.endswith('.json'):
-            with open(os.path.join(root, file), 'r') as json_file:
-                # add url to dictionary
-                json_obj = json.load(json_file)
-                id_url_map[index] = json_obj['url']
-                # parse html for content as a whole string
-                soup = BeautifulSoup(json_obj['content'], 'html.parser')
-                # find all visible text
-                texts = soup.findAll(text=True)
-                visible_texts = filter(tag_visible, texts)
-                # Also find all titles, headers and bold font
-                title = ''
-                h1 = ''
-                h2 = ''
-                h3 = ''
-                bold = ''
-                strong = ''
-                if soup.title != None:
-                    title = soup.title.text
-                if soup.h1 != None:
-                    h1 = soup.h1.text
-                if soup.h2 != None:
-                    h2 = soup.h2.text
-                if soup.h3 != None:
-                    h3 = soup.h3.text
-                if soup.b != None:
-                    bold = soup.b.text
-                if soup.strong != None:
-                    strong = soup.strong.text
-                important_words = ''
-                important_words += ' ' + title + ' ' + h1 + ' ' + h2 + ' ' + h3 + ' ' + bold + ' ' + strong
-                # format the text
-                content = " ".join(t.strip() for t in visible_texts)
-                content += 5 * important_words
-                # porter stemming
-                token_list = porter_stem(content)
-                # add token to token_map
-                add_token_to_map(token_list, index)
-                id_size_map[index] = len(token_list)
-                index += 1
-                temp_index += 1
-                if temp_index > 20000:
-                    file_name = "token_map" + str(file_number) + ".txt"
-                    write_map_to_file(token_map, file_name)
-                    token_map = {}
-                    temp_index = 1
-                    file_number += 1
-write_map_to_file(token_map, "token_map" + str(file_number) + ".txt")
 
-f = open("id_url.txt", "w")
-for key in sorted (id_url_map.keys()):
-    f.write(str(key) + ' ' + id_url_map[key] + '\n')
-f.close()
+def get_tf_idf():
+    vectorizer = CountVectorizer()
+    transformer = TfidfTransformer()
+    tfidf = transformer.fit_transform(vectorizer.fit_transform(corpus))
+    word = vectorizer.get_feature_names()
+    weight = tfidf.toarray()
+    f = open("tf-idf.txt", "w")
+    for i in range(len(weight)):
+        f.write("-----------Document: " + str(i + 1) + " tf - idf" + '-----------\n')
+        for j in range(len(word)):
+            if weight[i][j] != 0:
+                f.write(str(word[j]) + ' ' + str(weight[i][j]) + '\n')
+    f.close()
 
-f = open("id_size.txt", "w")
-for key in sorted (id_size_map.keys()):
-    f.write(str(key) + ' ' + str(id_size_map[key]) + '\n')
-f.close()
+if __name__ == '__main__':
+
+    id_url_map = {}
+    token_map = {}
+    index = 1
+    corpus = []
+    for root, dirs, files in os.walk('./DEV'):
+        for file in files:
+            if file.endswith('.json'):
+                with open(os.path.join(root, file), 'r') as json_file:
+                    # add url to dictionary
+                    json_obj = json.load(json_file)
+                    id_url_map[index] = json_obj['url']
+                    # parse html for content as a whole string
+                    soup = BeautifulSoup(json_obj['content'], 'html.parser')
+                    texts = soup.findAll(text=True)
+                    visible_texts = filter(tag_visible, texts)
+                    content = " ".join(t.strip() for t in visible_texts)
+                    corpus.append(content)
+
+                    # porter stemming
+                    # token_list = porter_stem(content)
+                    # write_doc_size_to_file(index, len(token_list))
+                    # print(token_list)
+                    # add_token_to_map(token_list, index)
+                    # index += 1
+    f = open("url_id.txt", "w")
+    for key, value in id_url_map.items():
+        f.write(str(key) + " " + value + '\n')
+    # get_tf_idf()
+    # beautiful_print_map(token_map)
+    # write_map_to_file()
